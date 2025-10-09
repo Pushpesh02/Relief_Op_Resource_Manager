d9a3ae24ed42318b034fc1130d197fd8d5baa816
Pushpesh02 <pushpeshcool91@gmail.com>
2025-10-09 19:50:34 +0530

    Update data_structures.h

--- data_structures.h
+++ data_structures.h
@@ -1,15 +1,34 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
 
-// Struct for a single resource item
+#define MAX_TRUCK_CAPACITY 10
+
+
 typedef struct Resource {
     char name[50];
     int quantity;
-    struct Resource *next; // For the Linked List
+    struct Resource *next; 
 } Resource;
 
-// Struct for a single victim
 typedef struct Victim {
     char name[50];
     int age;
-    char category[10]; // "child", "adult", "senior"
-    struct Victim *next; // For the Queue
+    char category[10]; 
+    struct Victim *next; 
 } Victim;
+
+typedef struct {
+    char item_name[50];
+    int quantity;
+} Supply;
+
+typedef struct {
+    Resource *resource_head;
+    Victim *queue_head;
+    Victim *queue_tail;
+    Supply truck_stack[MAX_TRUCK_CAPACITY];
+    int stack_top;
+} SystemState;
+
+
+#endif 
