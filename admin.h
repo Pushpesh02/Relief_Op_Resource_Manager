41aacca3ed770fc55c1733e90bf80b276fc44630
Pushpesh02 <pushpeshcool91@gmail.com>
2025-10-12 18:21:42 +0530

    Create admin.h

---
+++ admin.h
@@ -0,0 +1,10 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "resource.h"
+#include "victim.h"
+#include "truck.h"
+
+Resource* adminMenu(Resource* resHead, VictimQueues* vqs, TruckStack* trucks);
+
+#endif
