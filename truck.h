a7afcd4f8aa7085675fcb291161c6e3ed76f8c28
Pushpesh02 <pushpeshcool91@gmail.com>
2025-11-28 18:08:04 +0530

    Update truck.h

--- truck.h
+++ truck.h
@@ -1,7 +1,7 @@
 #ifndef TRUCK_H
 #define TRUCK_H
 
-#define STACK_MAX 20
+#define STACK_MAX 50
 
 typedef struct {
     int id;
@@ -11,14 +11,14 @@ typedef struct {
 
 typedef struct {
     TruckLoad data[STACK_MAX];
-    int top; 
+    int top;
 } TruckStack;
 
 void initTruckStack(TruckStack* s);
-int pushTruck(TruckStack* s, int id, char item[], int qty); 
+int pushTruck(TruckStack* s, int id, char item[], int qty);
 int popTruck(TruckStack* s, TruckLoad* out);
 void displayTrucks(TruckStack* s);
-
-
+int saveTrucks(TruckStack* s, char path[]);
+int loadTrucks(TruckStack* s, char path[]);
 
 #endif
