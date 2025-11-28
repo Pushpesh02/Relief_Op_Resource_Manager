8253defd9e6f3c7ef8bb35f2818d20c27f3366f3
Pushpesh02 <pushpeshcool91@gmail.com>
2025-11-28 18:05:51 +0530

    Refactor admin login and update auth menu options

--- auth.c
+++ auth.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#include "auth.h"
 #include "admin.h"
 #include "user.h"
+#include "resource.h"
+#include "victim.h"
+#include "truck.h"
 
+/* simple admin login (username/password) */
 int adminLogin() {
     char u[50], p[50];
     printf("Username: "); scanf("%49s", u);
     printf("Password: "); scanf("%49s", p);
-    if (strcmp(u, "admin") == 0 && strcmp(p, "1234") == 0) {
-        printf("Login success.\n");
-        return 1;
-    }
-    printf("Invalid credentials.\n");
-    return 0;
+    if (strcmp(u, "admin") == 0 && strcmp(p, "1234") == 0) { printf("Login success.\n"); return 1; }
+    printf("Invalid admin credentials.\n"); return 0;
 }
 
-void authMenu(Resource** res, VictimQueues* vq, TruckStack* trucks) {
+void authMenu(Resource** pres, VictimQueues* pvq, TruckStack* ptrucks) {
     int ch = 0;
     while (1) {
-        printf("\nLogin as:\n1. Admin\n2. User\n3. Exit\nChoice: ");
-        if (scanf("%d", &ch) != 1) { while (getchar()!='\n'); 
-            ch = 0; }
-
+        printf("\nLogin as:\n1. Admin\n2. User\n3. Save All & Exit\n4. Exit without Save\nChoice: ");
+        if (scanf("%d", &ch) != 1) { while (getchar()!='\n'); ch = 0; }
         if (ch == 1) {
             if (adminLogin()) {
-                *res = adminMenu(*res, vq, trucks);
+                *pres = adminMenu(*pres, pvq, ptrucks);
             }
         } else if (ch == 2) {
-            *res = userMenu(*res, vq);
+            *pres = userMenu(*pres, pvq);
         } else if (ch == 3) {
-            printf("Goodbye.\n");
+            saveResources(*pres, "resources.txt");
+            saveVictims(pvq, "victims.txt");
+            saveTrucks(ptrucks, "trucks.txt");
+            printf("Saved all. Exiting.\n");
             break;
+        } else if (ch == 4) {
+            printf("Exiting without save.\n"); break;
         } else {
             printf("Invalid choice.\n");
         }
